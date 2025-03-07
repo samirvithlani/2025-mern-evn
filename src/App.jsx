75552783ad6063b2e79ad31eabe0e2ccad229dda
samirvithlani <samir.vithlani83955@gmail.com>
2025-03-07 19:58:34 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+  import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+      <h1>HELLO ALL</h1>
+      <h2>Every tag must have closing tag in react js</h2>
+      <h2>At a time we can return only 1 element and 1 element should return</h2>
+      <h3>whatever has been written inside return will display on browser..</h3>
+      
+   </div>
+  )
+}
+
+export default App
